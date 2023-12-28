023526d1f0ef3a5efe989c1ff07942951fff4215
cheesecrackers2544 <153966412+cheesecrackers2544@users.noreply.github.com>
2023-12-28 20:15:34 +1300

    Delete wordpresstheme directory

--- wordpresstheme/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-<?php
-function firstday_theme_scripts() {
-    wp_enqueue_style( 'firstday-style', get_stylesheet_uri() );
-}
-
-add_action( 'wp_enqueue_scripts', 'firstday_theme_scripts' );
--- wordpresstheme/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<main class="main-content">
-    <div class="search-container">
-        <?php get_search_form(); ?>
-    </div>
-</main>
-
-<?php get_footer(); ?>
