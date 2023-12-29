78d2ab62e2be8e693769566ec731e069f8f33fd2
David <jackbunckenburg@gmail.com>
2023-12-29 13:59:37 +1300

    adding website with no CMS and updated SQL schema

---
+++ databasedirectory/scripts/firstday database script2.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS wp_commentmeta,
+                     wp_comments,
+                     wp_e_events,
+                     wp_links,
+                     wp_options,
+                     wp_postmeta,
+                     wp_posts,
+                     wp_term_relationships,
+                     wp_term_taxonomy,
+                     wp_termmeta,
+                     wp_terms,
+                     wp_usermeta,
+                     wp_users;
+
